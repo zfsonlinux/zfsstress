5d7b8c0882b9b611c00407dba2965e8c53ef15b2
Olaf Faaland <faaland1@llnl.gov>
2016-03-30 22:05:03 -0700

    Add runmany() to automate running a script
    runmany <count> <cmd> runs a script <count> times,
    directing stdout and stderr to ${LOGDIR}/${CMD}.$x.log
    where $x is the instance number from 1..<count>.
    
    Use this in runstress to eliminate redundant code
    and make it easier to increase the number of instances
    of a given script that are run.

--- runstress.sh
+++ runstress.sh
@@ -11,38 +11,29 @@ fi
 pushd $basedir > /dev/null
 
 # Run more instances to increase stress level.
-./randcreate.sh < /dev/null > ${LOGDIR}/randcreate.log 2>&1 &
-./randcreate.sh < /dev/null > ${LOGDIR}/randcreate2.log 2>&1 &
 
-./randwrite.sh < /dev/null > ${LOGDIR}/randwrite.log 2>&1 &
-./randwrite.sh < /dev/null > ${LOGDIR}/randwrite2.log 2>&1 &
+# User operations
+runmany 2 ./randcreate.sh
+runmany 2 ./randwrite.sh
+runmany 2 ./randrm.sh
+runmany 2 ./randxattr.sh
+runmany 2 ./randsymlink.sh
+runmany 2 ./randmkdir.sh
 
-./randrm.sh < /dev/null > ${LOGDIR}/randrm.log 2>&1 &
-./randrm.sh < /dev/null > ${LOGDIR}/randrm2.log 2>&1 &
-
-./randxattr.sh < /dev/null > ${LOGDIR}/randxattr.log 2>&1 &
-./randxattr.sh < /dev/null > ${LOGDIR}/randxattr2.log 2>&1 &
-
-./randsymlink.sh < /dev/null > ${LOGDIR}/randsymlink.log 2>&1 &
-./randsymlink.sh < /dev/null > ${LOGDIR}/randsymlink2.log 2>&1 &
-
-./randmkdir.sh < /dev/null > ${LOGDIR}/randmkdir.log 2>&1 &
-./randmkdir.sh < /dev/null > ${LOGDIR}/randmkdir2.log 2>&1 &
-
-# Pool operations.
-./randimportexport.sh < /dev/null > ${LOGDIR}/randimportexport.log 2>&1 &
-./randscrub.sh < /dev/null > ${LOGDIR}/randscrub.log 2>&1 &
+## Pool operations.
+runmany 1 ./randimportexport.sh
+runmany 1 ./randscrub.sh
 
 # Dataset operations.
-./randsnapshot.sh < /dev/null > ${LOGDIR}/randsnapshot.log 2>&1 &
-./randdataset.sh < /dev/null > ${LOGDIR}/randdataset.log 2>&1 &
-./randsendrecv.sh < /dev/null > ${LOGDIR}/randsendrecv.log 2>&1 &
+runmany 1 ./randsnapshot.sh
+runmany 1 ./randdataset.sh
+runmany 1 ./randsendrecv.sh
 
 # Randomly change dataset properties.
-./randproprecordsize.sh < /dev/null > ${LOGDIR}/randproprecordsize.log 2>&1 &
-./randpropdnodesize.sh < /dev/null > ${LOGDIR}/randpropdnodesize.log 2>&1 &
-./randpropxattr.sh < /dev/null > ${LOGDIR}/randpropxattr.log 2>&1 &
-./randpropcompression.sh < /dev/null > ${LOGDIR}/randpropcompression.log 2>&1 &
+runmany 1 ./randproprecordsize.sh
+runmany 1 ./randpropdnodesize.sh
+runmany 1 ./randpropxattr.sh
+runmany 1 ./randpropcompression.sh
 
 popd > /dev/null
 
